4bad5ad219eb5381913772b1d5183c97817e10cb
Daniel Hamm <7501287+daniel-hamm@users.noreply.github.com>
2022-12-18 00:03:30 +0100

    Implement the random function

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function getComputerChoice() {
-    // assign a random number from 0 to 2 to a variable
+    // assign a random number from 1 to 3 to a variable
+    let random_number = Math.floor(Math.random() * 3) + 1;
     // switch case to choose the desired output with the previously generated number
-    // return rock if number = 0
-    // return paper if number = 1
-    // return scissors if number = 2
+    // return rock if number = 1
+    // return paper if number = 2
+    // return scissors if number = 3
+    console.log(random_number);
 }
