a5be393c0992aece412b089a2fc7be96fca5f3c6
Daniel Hamm <7501287+daniel-hamm@users.noreply.github.com>
2022-12-17 23:58:41 +0100

    Add pseudocode for the first function

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    // assign a random number from 0 to 2 to a variable
+    // switch case to choose the desired output with the previously generated number
+    // return rock if number = 0
+    // return paper if number = 1
+    // return scissors if number = 2
+}
