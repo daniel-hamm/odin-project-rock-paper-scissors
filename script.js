7f285692678a47fa6f63f54fb4ac6628f3b1dab9
Daniel Hamm <7501287+daniel-hamm@users.noreply.github.com>
2022-12-18 00:08:39 +0100

    Implement switch case

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 function getComputerChoice() {
     // assign a random number from 1 to 3 to a variable
     let random_number = Math.floor(Math.random() * 3) + 1;
+
     // switch case to choose the desired output with the previously generated number
-    // return rock if number = 1
-    // return paper if number = 2
-    // return scissors if number = 3
-    console.log(random_number);
+    switch(random_number) {
+        // return rock if number = 1
+        case 1: 
+            return "Rock";
+            break;
+        // return paper if number = 2
+        case 2:
+            return "Paper";
+            break;
+        // return scissors if number = 3
+        case 3:
+            return "Scissors";
+            break;
+        // return an error if random fails
+        // this case should never happen
+        default:
+            return "Error";
+            break;
+    }
 }
